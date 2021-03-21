01d0d48927a7a3956285bada6ee6d36983a62d4f
mateusdeitos <mate.deitos@hotmail.com>
2021-03-21 15:20:37 -0300

    first commit

---
+++ react/dtmoney/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
+
+
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
+  );
+}
