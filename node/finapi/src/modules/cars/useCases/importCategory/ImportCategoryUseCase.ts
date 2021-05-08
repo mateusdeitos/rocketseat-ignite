9a8fc26a36f704ff2f7431f83773c6524fe501f2
mateusdeitos <mate.deitos@hotmail.com>
2021-05-08 09:58:17 -0300

    Remover arquivo do tmp ao terminar de ler arquivo

--- node/finapi/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ node/finapi/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -26,7 +26,10 @@ export class ImportCategoryUseCase {
 					const [name, description] = line;
 					categories.push({ name, description });
 				})
-				.on("end", () => resolve(categories))
+				.on("end", () => {
+					fs.promises.unlink(file.path);
+					resolve(categories);
+				})
 				.on("error", (error) => reject(error));
 		})
 	}
