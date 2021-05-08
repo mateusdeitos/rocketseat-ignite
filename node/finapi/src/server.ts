e6eaa8af62c73fe5e49203ddc4df27beabe99683
mateusdeitos <mate.deitos@hotmail.com>
2021-05-08 09:56:16 -0300

    Swagger UI para docs

--- node/finapi/src/server.ts
+++ node/finapi/src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import { routes } from './routes';
+import swaggerUI from 'swagger-ui-express';
+import swaggerFIle from './swagger.json';
 
 const app = express();
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFIle, {explorer: true}))
 
 app.use(routes);
 
