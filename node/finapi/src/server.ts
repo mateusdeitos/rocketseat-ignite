4a6ec733f4126c2b26ad57234318869ec906d51f
mateusdeitos <mate.deitos@hotmail.com>
2021-05-08 12:16:56 -0300

    melhorando docs

--- node/finapi/src/server.ts
+++ node/finapi/src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { routes } from './routes';
 import swaggerUI from 'swagger-ui-express';
-import swaggerFIle from './docs/swagger';
+import swaggerFactory from './docs/swagger';
 
 const app = express();
 app.use(express.json());
 
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFIle()))
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFactory()))
 
 app.use(routes);
 
