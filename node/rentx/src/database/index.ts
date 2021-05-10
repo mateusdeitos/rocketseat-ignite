145398ae5f5b50a74390ee8c359c5c08fadab00b
mateusdeitos <mate.deitos@hotmail.com>
2021-05-10 08:21:17 -0300

    Explicação da gambi

--- node/rentx/src/database/index.ts
+++ node/rentx/src/database/index.ts
@@ -5,6 +5,7 @@ interface IOptions {
 }
 
 getConnectionOptions().then(options => {
+	// É gambi, o host de options é somente leitura, então é criado um novo obj baseado no options e alterado o host. 'newOptions' é uma referência à options, então ao alterar o host de 'newOptions', o host em 'options' tmb é alterado
 	const newOptions = options as IOptions;
 	newOptions.host = 'database_main'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
 	createConnection({
