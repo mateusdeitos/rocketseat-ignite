50eee77549cce6d6b04d7b478b2a9fa14b53b295
mateusdeitos <mate.deitos@hotmail.com>
2021-05-10 19:01:28 -0300

    Migration Categories

--- node/rentx/src/modules/cars/entities/Category.ts
+++ node/rentx/src/modules/cars/entities/Category.ts
@@ -1,11 +1,21 @@
 
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
+@Entity('categories')
 export class Category {
-	name: string;
-	description: string;
+	@PrimaryColumn()
 	id?: string;
+
+	@Column()
+	name: string;
+
+	@Column()
+	description: string;
+
+	@CreateDateColumn()
 	created_at?: Date;
+
 	constructor() {
 		if (!this.id) this.id = uuid();
 		if (!this.created_at) this.created_at = new Date();
