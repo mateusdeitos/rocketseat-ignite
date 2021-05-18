2f7f5ee77e0a9cc0474e437c768f92ba9161be14
mateusdeitos <mate.deitos@hotmail.com>
2021-05-18 07:44:21 -0300

    Middleware de admin

--- node/rentx/src/shared/infra/http/routes/cars.routes.ts
+++ node/rentx/src/shared/infra/http/routes/cars.routes.ts
@@ -2,9 +2,10 @@ import { Router } from 'express';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateCarController } from '@modules/cars/useCases/createCar/createCarController';
 import { ListCarsController } from '@modules/cars/useCases/listCars/listCarsController';
+import { ensureAdmin } from '../middlewares/ensureAdmin';
 
 export const carsRoutes = Router();
 const createCarController = new CreateCarController();
 const listCarsController = new ListCarsController();
 carsRoutes.get('/', ensureAuthenticated, listCarsController.handle);
-carsRoutes.post('/', ensureAuthenticated, createCarController.handle);
+carsRoutes.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
